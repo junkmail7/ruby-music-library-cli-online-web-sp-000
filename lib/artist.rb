ed6e352af4b3a05cf4b444e42cbafbb3da2a20dc
William Pannell <lpann5@gmail.com>
2021-03-17 00:28:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
     attr_accessor :name
     attr_reader :songs
     @@all = []
-    def initialize(name, song = nil, genre = nil)
+    def initialize(name)
         @name = name
         @songs = []
     end
